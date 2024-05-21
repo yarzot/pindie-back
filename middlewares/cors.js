d03c82dbcd0edf0059df6c9610faebad6637f8c9
yarzot <yarozot71@gmail.com>
2024-05-21 14:19:38 +0300

    okk

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -2,16 +2,20 @@ const allowedCors = [
     'https://practicum.yandex.ru',
     'https://students-projects.ru',
     'localhost:3000',
-    'localhost:3001'
+    'localhost:3001',
+    'shishkina.nomoredomainswork.ru',
+    'pindie-frontend.nomoredomainswork.ru'
 ];
   
 function cors(req, res, next) {
-    const { origin } = req.headers; 
-    if (allowedCors.includes(origin)) {
-      res.header('Access-Control-Allow-Origin', origin);
-    }
-  
-    next();
-};
+  const { origin } = req.headers;
+
+  if (allowedCors.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+  next();
+}
   
 module.exports = cors;
