6d68dfbec54aeaef6057f47e675dba0ad32c3565
yarzot <yarozot71@gmail.com>
2024-05-28 08:24:26 +0300

    hh

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -6,7 +6,8 @@ function cors(req, res, next) {
   if (allowedCors.includes(origin)) {
     res.header("Access-Control-Allow-Origin", origin);
   }
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,PATCH,HEAD");
+
   next();
 }
   
