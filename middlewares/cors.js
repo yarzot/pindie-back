87928923b4c9f46f81b0143ad6f870c4104a271d
yarzot <yarozot71@gmail.com>
2024-05-28 15:18:02 +0300

    rffr

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,11 +1,6 @@
 const allowedCors = ["https://shishkina.nomoredomainswork.ru", "https://zotov.nomoredomainswork.ru"];
   
 function cors(req, res, next) {
-  const { origin } = req.headers;
-
-  if (allowedCors.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
-  }
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,PATCH,HEAD");
   res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
