a8b5dd229151030f5b2b975188dbedc991052c91
yarzot <yarozot71@gmail.com>
2024-05-29 20:09:07 +0300

    z

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -6,11 +6,11 @@ const allowedCors = [
 
   ];
 function cors(req, res, next) {
-    if (allowedCors.includes(origin)) {
-        res.header('Access-Control-Allow-Origin', origin)
-        res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE')
-        res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization')
-    }
+   const { origin } = req.headers;
+  res.header("Access-Control-Allow-Origin", origin);
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+  next();
 }
 module.exports = {
     cors
