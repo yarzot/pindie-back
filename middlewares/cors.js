1b1a64cfab71937887328e6c22fa06bb24c702e7
yarzot <yarozot71@gmail.com>
2024-05-29 21:53:15 +0300

    d

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,7 +1,5 @@
 const allowedCors = [
-    'https://practicum.yandex.ru',
-    'https://students-projects.ru',
-    'http://localhost:3000/',
+    '*',
     'https://zotov.nomorepartiesco.ru/'
 ];
   
